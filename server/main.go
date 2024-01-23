44e6c9388ac87e7f5d5391f2bd0a75171c49c354
khoahuynhdev <khoa.hd96@gmail.com>
2024-01-23 21:49:14 +0700

    docs: add question about rpc expose

--- server/main.go
+++ server/main.go
@@ -24,6 +24,9 @@ func main() {
 
 	// register RPC server
 	rpc.Register(timeServer)
+
+	// NOTE: can I register just a function, no method from object or receiver func?
+	// short: kinda can't, ref: https://go.dev/src/net/rpc/server.go
 	rpc.HandleHTTP()
 	// listen for request on port 1234
 	l, e := net.Listen("tcp", ":1234")
